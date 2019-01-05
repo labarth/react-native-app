473cc13d233ba8a490754468b3511f617a226ed1
alexander matiugin <alexander.matiugin@pandadoc.com>
2019-01-05 10:39:36 +0300

    first commit2

--- configureStore.js
+++ configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import reducer from './src/redux/rootReducer';
+import reducer from './xyu/redux/rootReducer';
 
 const enhancer = applyMiddleware(thunk, logger);
 
